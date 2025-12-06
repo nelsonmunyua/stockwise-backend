634ba0a683b409cbdac7c9019ccb1e0b8e087273
Nelson Munyua <nelsonmunyua8@gmail.com>
2025-12-06 09:55:24 +0300

    updated frontend link

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ origins = [
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
-    "https://stockwise-frontend-weld.vercel.app/"
+    "https://stockwise-frontend-weld.vercel.app"
 ]
 
 app.add_middleware(
